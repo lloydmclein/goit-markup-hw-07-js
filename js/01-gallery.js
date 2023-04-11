e63e1b704551fd2aee3abc1e02023ba13477e7c8
lloydmclein <lloydmclein@gmail.com>
2023-04-11 14:14:09 +0200

    done Task#1
    done Task#1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,2 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+const gallery = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+gallery.addEventListener("click", onGalleryClick);
+
+
+
+
+
+function createGalleryMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+      </a>
+         </div>`
+        }).join("");
+}
+
+function onGalleryClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") return;
+    
+    const instance = basicLightbox.create(
+        `<img 
+           src="${event.target.dataset.source}" 
+           width="800"
+           height="600">`,
+        {
+            onShow: () => window.addEventListener("keydown", escapeClose),
+            onclose: () => window.removeEventListener("keydown", escapeClose),
+        }
+    );
+    instance.show();
+
+    function escapeClose(evt) {
+        if (evt.code === "Escape") {
+            instance.close();
+        }
+    }
+}
