434e5518d68a75167203c3ce6ca8c9f5fdd62dbc
lloydmclein <lloydmclein@gmail.com>
2023-04-11 14:36:51 +0200

    Done task#2
    Done task#2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,11 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-const gallery = galleryItems.map(({ preview, description }) => 
-    `<div class="gallery__item">
-   <img class="gallery__image" src="${preview}" alt="${description}"></div>`
-).join("");
 
-const galleryPage = document.querySelector(".gallery");
-galleryPage.innerHTML = gallery;
+const gallery = document.querySelector(".gallery");
+const lightBoxMarkup = createLightboxMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", lightBoxMarkup);
 
-console.log(gallery);
+
+function createLightboxMarkup(items) {
+    return items
+        .map(({ preview, original, description }) => {
+            return ` <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+      src="${preview}" 
+      alt="${description}" />
+      </a> `
+        }).join("");
+}
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
